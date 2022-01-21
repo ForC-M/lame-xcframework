611d9bc77b1e9a67f8ecff866469753973d50ad9
ForC <heqiao@youzan.com>
2022-01-21 16:40:22 +0800

    fix: 语法错误

--- build.sh
+++ build.sh
@@ -1,29 +1,21 @@
-#!/bin/zsh
+#!/usr/bin/env zsh
 set -e
 
 # for m1
-ARCH="arm64 arm64 x86_64 armv7 armv7s i386"
+ARCH="arm64 arm64 x86_64 armv7 armv7s"
 SOURCE_PATH=`pwd`/xcframework
-
 arm64_iphone=0
+
 for arch in $ARCH 
 do
-    if [ "$arch" = "i386" -o "$arch" = "x86_64" ]
-    then
+    if [ "$arch" == "x86_64" ];then
 		PLATFORM="iphonesimulator"
-		if [ "$arch" = "x86_64" ]
-		then
-		    SIMULATOR="-mios-simulator-version-min=7.0"
-            HOST=x86_64-apple-darwin
-		else
-		    SIMULATOR="-mios-simulator-version-min=5.0"
-            HOST=i386-apple-darwin
-		fi
-	elif [ "$arch" = "arm64" ]
+		SIMULATOR="-mios-simulator-version-min=7.0"
+        HOST=x86_64-apple-darwin
+	elif [ "$arch" == "arm64" ];then
         HOST=arm-apple-darwin
         SIMULATOR="-miphoneos-version-min=7.0"
-        if [ $arm64_iphone == 0 ]
-        then
+        if [ $arm64_iphone == 0 ];then
             PLATFORM="iphoneos"
             arm64_iphone=1
         else
@@ -34,18 +26,18 @@ do
        SIMULATOR="-miphoneos-version-min=7.0"
        PLATFORM="iphoneos"
 	fi
-
     ./configure \
-    --disable-shared --disable-frontend \
+    --disable-shared \
+    --disable-frontend \
     --host=$HOST \
-    --prefix="$SOURCE_PATH" \
+    --prefix="$SOURCE_PATH/$PLATFORM/$arch" \
     CC="xcrun -sdk $PLATFORM clang -arch $arch" \
-    CFLAGS="-arch $arch -fembed-bitcode $SIMULATOR" \
-    LDFLAGs="-arch $arch -fembed-bitcode $SIMULATOR"
+    CFLAGS="-fembed-bitcode-marker $SIMULATOR" \
+    LDFLAGs="-fembed-bitcode-marker $SIMULATOR"
     make clean
     make -j8
     make install
-fi
+done
 
 
 
